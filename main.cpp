3e623c3c2196cb4b275a459ff7538e3012541c91
student <@>
2022-10-07 11:43:18 +0200

    Zmiana mnożenia na mnożenie nawiasów w metodzie Policz

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int Policz(int a, int b){
-    return a * b;
+    return (a+b) * (a-b);
 }
 
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
-    cout<<"W drugim branchu; 2 * 3 = "<<Policz(2, 3)<<endl;
+    cout<<"W drugim branchu; (2+3)*(2-3) = "<<Policz(2, 3)<<endl;
     return 0;
 }
